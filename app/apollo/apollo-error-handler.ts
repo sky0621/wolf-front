7fd8d4b9f001d3894fd64b5e583355fa629a95e4
sky0621 <sky14work@gmail.com>
2020-06-29 00:08:58 +0900

    init

--- app/apollo/apollo-error-handler.ts
+++
@@ -1,7 +0,0 @@
-export default (error: any, context: any) => {
-  const { networkError } = error
-  if (networkError && networkError.statusCode === 401) {
-    location.href = '/login'
-  }
-  context.error({ statusCode: 304, message: 'Server error' })
-}
